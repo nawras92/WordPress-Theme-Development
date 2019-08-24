3166945d0cd0b151df0d68dd4c696a10ff38859b
Nawras Ali <nawras@learnwithnaw.net>
2019-08-24 12:45:36 +0300

    add post template

---
+++ wp-content/themes/lwn/single.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+        <section>
+            <?php if (have_posts()): ?>
+                <?php while (have_posts()):?>
+                    <?php the_post(); ?>
+                    <article>
+                        <h3><?php the_title(); ?></h3>
+                        <span>
+                          Published on:   <?php the_date(); ?>
+                          By: <?php the_author_posts_link(); ?>
+                          In: <?php the_category(', '); ?>
+                          <?php the_tags('| ', ', ', ' |'); ?>
+                        </span>
+                        <?php the_content(); ?>
+                    </article>
+                <?php endwhile; ?>
+            <?php else: ?>
+                <?php echo "There are no posts"; ?>
+            <?php endif; ?>
+
+
+        </section>
+
+        <?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
