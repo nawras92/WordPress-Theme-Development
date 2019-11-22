a7f7ddfb68227e9df8406e95c93f0876ab3252f7
Nawras Ali <nawras@learnwithnaw.net>
2019-11-22 14:08:50 +0200

    add static footer widgets

--- wp-content/themes/lwn/footer.php
+++ wp-content/themes/lwn/footer.php
@@ -1,10 +1,40 @@
-
-        <footer>
-             All copyrights reserved to LWN THEME
-        </footer>
+<footer id="main-footer">
+    <div class="container footer-container">
+        <div class="footer-column">
+            <div class="lwn-widget">
+                <h4>About the website</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga necessitatibus fugiat, sed, ut quam non voluptate eaque iusto beatae vitae libero tempore incidunt nostrum quas iste. Porro architecto officia explicabo.</p>
+            </div>
+        </div>
+        <div class="footer-column">
+            <div class="lwn-widget">
+                <h4>Some useful links</h4>
+                <ul>
+                    <li><a href="#">link 1</a></li>
+                    <li><a href="#">link 2</a></li>
+                    <li><a href="#">link 3</a></li>
+                    <li><a href="#">link 4</a></li>
+                </ul>            
+            </div>
+        </div>
+        <div class="footer-column">
+            <div class="lwn-widget">
+                <h4>important articles</h4>
+                <ul>
+                    <li><a href="#">Lorem ipsum dolor sit amet</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-column">
+            <p>copyright lwn theme 2019</p>
+        </div>
+    </div>
+</footer>
 
         <?php wp_footer(); ?>
-
     </body>
-
 </html>
