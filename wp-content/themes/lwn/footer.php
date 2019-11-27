129834e4c334e88591d08d560b4a46f099f5170f
Nawras Ali <nawras@learnwithnaw.net>
2019-11-27 14:04:40 +0200

    add dynamic footer sidebars

--- wp-content/themes/lwn/footer.php
+++ wp-content/themes/lwn/footer.php
@@ -1,33 +1,19 @@
 <footer id="main-footer">
     <div class="container footer-container">
         <div class="footer-column">
-            <div class="lwn-widget">
-                <h4>About the website</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga necessitatibus fugiat, sed, ut quam non voluptate eaque iusto beatae vitae libero tempore incidunt nostrum quas iste. Porro architecto officia explicabo.</p>
-            </div>
+            <?php if (is_active_sidebar('footer1')): ?>
+                <?php dynamic_sidebar('footer1'); ?>
+            <?php endif; ?>
         </div>
         <div class="footer-column">
-            <div class="lwn-widget">
-                <h4>Some useful links</h4>
-                <ul>
-                    <li><a href="#">link 1</a></li>
-                    <li><a href="#">link 2</a></li>
-                    <li><a href="#">link 3</a></li>
-                    <li><a href="#">link 4</a></li>
-                </ul>            
-            </div>
+        <?php if (is_active_sidebar('footer2')): ?>
+                <?php dynamic_sidebar('footer2'); ?>
+            <?php endif; ?>
         </div>
         <div class="footer-column">
-            <div class="lwn-widget">
-                <h4>important articles</h4>
-                <ul>
-                    <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-                    <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-                    <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-                    <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-                    <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-                </ul>
-            </div>
+            <?php if (is_active_sidebar('footer3')): ?>
+                    <?php dynamic_sidebar('footer3'); ?>
+            <?php endif; ?>
         </div>
         <div class="footer-column">
             <p>copyright lwn theme 2019</p>
