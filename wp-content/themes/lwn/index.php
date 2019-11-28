01a3d7618226ea893c4cc69e624d937bda591aeb
Nawras Ali <nawras@learnwithnaw.net>
2019-11-28 22:37:38 +0200

    Add wordpress loop

--- wp-content/themes/lwn/index.php
+++ wp-content/themes/lwn/index.php
@@ -6,33 +6,40 @@
             <p><?php bloginfo('description'); ?></p>
     </header>
  
-
-
-<section>
-            <h2>Blog Posts</h2>
-
-
-            <?php if (have_posts()): ?>
-                <?php while (have_posts()):?>
+    <!-- Blog posts -->
+    <section id="blog-posts">
+        <div class="container posts-container">
+            <?php if(have_posts()): ?>
+                <?php while(have_posts()): ?>
                     <?php the_post(); ?>
-                    <article>
-                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                        <span>
-                          Published on: <?php the_date(); ?>
-                          By: <?php the_author_posts_link(); ?>
-                          In: <?php the_category(', '); ?>
-                          <?php the_tags('| ', ', ', ' |'); ?>
-                        </span>
-                        <?php the_excerpt(); ?>
-                    </article>
-                <?php endwhile; ?>
+                        <article class="card">
+                            <?php if(has_post_thumbnail()): ?>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_post_thumbnail('medium'); ?>                          
+                                </a>
+                            <?php else: ?>
+                                <a href="<?php the_permalink(); ?>">
+                                <img src="https://via.placeholder.com/300" alt="">
+                                </a>
+                            <?php endif; ?>
+                            <div>
+                                <h3>
+                                    <a href="<?php the_permalink(); ?>">
+                                      <?php the_title(); ?>
+                                    </a>
+                                </h3>
+                                <p><?php the_excerpt(); ?></p>
+                                <div class="post-categories">
+                                    <?php the_category(' '); ?>
+                                </div>
+                            </div>
+                        </article>
+                <?php endwhile;?>
             <?php else: ?>
-                <?php echo "There are no posts"; ?>
+                <?php echo "There are NO posts."; ?>
             <?php endif; ?>
+        </div>
+    </section>
 
-
-        </section>
-
-        <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
