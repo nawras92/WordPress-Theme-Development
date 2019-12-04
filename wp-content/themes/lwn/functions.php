a2203807acab74dd5b3ecb0db4aed2ef2369f241
Nawras Ali <nawras@learnwithnaw.net>
2019-12-04 15:50:34 +0200

    add sidebar widgets

--- wp-content/themes/lwn/functions.php
+++ wp-content/themes/lwn/functions.php
@@ -22,6 +22,17 @@ add_action('init', 'lwn_register_menus');
 
 // LWN widgets
 function lwn_register_widgets(){
+    $sidebar = array(
+        'name' => 'Sidebar',
+        'id' => 'sidebar',
+        'before_widget'=> '<div id="%1$s" class="widget card lwn-widget %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title'=> '</h4>'
+
+);
+    register_sidebar($sidebar);
+ 
     $footer1 = array(
                'name' => 'Footer Column 1',
                'id' => 'footer1',
