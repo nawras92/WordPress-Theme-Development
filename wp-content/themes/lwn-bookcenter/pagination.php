8ad3b2788f339ccc5dd1800bf8294c69fc4752bb
Nawras Ali <nawras@learnwithnaw.net>
2020-02-02 13:43:05 +0200

    add pagination template

---
+++ wp-content/themes/lwn-bookcenter/pagination.php
@@ -0,0 +1,10 @@
+<!-- Pagination -->
+
+<div id="pagination">
+    <div class="nav-previous">
+        <?php previous_posts_link('Newer Posts'); ?>
+    </div>
+    <div class="nav-next">
+        <?php next_posts_link('Older Posts');?>
+    </div>
+</div>
