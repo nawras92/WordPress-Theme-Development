d2e212b556439250ea3cec1552dde3a24b0feacf
Nawras Ali <nawras@learnwithnaw.net>
2020-02-05 09:56:13 +0200

    register custom taxonomies

--- wp-content/themes/lwn-bookcenter/functions.php
+++ wp-content/themes/lwn-bookcenter/functions.php
@@ -11,6 +11,102 @@ add_action('wp_enqueue_scripts', 'lwn_bookcenter_enqueue_stylesheets');
 add_theme_support('post-thumbnails');
 
 
+// Register custom taxonomies (book_type, writer, publisher)
+function lwn_bookcenter_register_custom_taxonomies() {
+	// Register book_type
+	$booktype_labels = array(
+		'name'              => _x( 'Book Types', 'taxonomy general name', 'lwn-bookcenter' ),
+		'singular_name'     => _x( 'Book Type', 'taxonomy singular name', 'lwn-bookcenter' ),
+		'search_items'      => __( 'Search Book Types', 'lwn-bookcenter' ),
+		'all_items'         => __( 'All Book Types', 'lwn-bookcenter' ),
+		'parent_item'       => __( 'Parent Book Type', 'lwn-bookcenter' ),
+		'parent_item_colon' => __( 'Parent Book Type:', 'lwn-bookcenter' ),
+		'edit_item'         => __( 'Edit Book Type', 'lwn-bookcenter' ),
+		'update_item'       => __( 'Update Book Type', 'lwn-bookcenter' ),
+		'add_new_item'      => __( 'Add New Book Type', 'lwn-bookcenter' ),
+		'new_item_name'     => __( 'New Book Type Name', 'lwn-bookcenter' ),
+		'menu_name'         => __( 'Book Type', 'lwn-bookcenter' ),
+	);
+
+	$booktype_args = array(
+		'hierarchical'      => true,
+		'labels'            => $booktype_labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => array( 'slug' => 'book-type' ),
+	);
+
+	register_taxonomy( 'book_type', array( 'book' ), $booktype_args );
+
+	// Register Writer Taxonomy
+	$writer_labels = array(
+		'name'                       => _x( 'Writers', 'taxonomy general name', 'lwn-bookcenter' ),
+		'singular_name'              => _x( 'Writer', 'taxonomy singular name', 'lwn-bookcenter' ),
+		'search_items'               => __( 'Search Writers', 'lwn-bookcenter' ),
+		'popular_items'              => __( 'Popular Writers', 'lwn-bookcenter' ),
+		'all_items'                  => __( 'All Writers', 'lwn-bookcenter' ),
+		'parent_item'                => null,
+		'parent_item_colon'          => null,
+		'edit_item'                  => __( 'Edit Writer', 'lwn-bookcenter' ),
+		'update_item'                => __( 'Update Writer', 'lwn-bookcenter' ),
+		'add_new_item'               => __( 'Add New Writer', 'lwn-bookcenter' ),
+		'new_item_name'              => __( 'New Writer Name', 'lwn-bookcenter' ),
+		'separate_items_with_commas' => __( 'Separate writers with commas', 'lwn-bookcenter' ),
+		'add_or_remove_items'        => __( 'Add or remove writers', 'lwn-bookcenter' ),
+		'choose_from_most_used'      => __( 'Choose from the most used writers', 'lwn-bookcenter' ),
+		'not_found'                  => __( 'No writers found.', 'lwn-bookcenter' ),
+		'menu_name'                  => __( 'Writers', 'lwn-bookcenter' ),
+	);
+
+	$writer_args = array(
+		'hierarchical'          => false,
+		'labels'                => $writer_labels,
+		'show_ui'               => true,
+		'show_admin_column'     => true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var'             => true,
+		'rewrite'               => array( 'slug' => 'writer' ),
+	);
+
+    register_taxonomy( 'writer', 'book', $writer_args );
+    
+    // Register Publisher Taxonomy
+	$publisher_labels = array(
+		'name'                       => _x( 'Publishers', 'taxonomy general name', 'lwn-bookcenter' ),
+		'singular_name'              => _x( 'Publisher', 'taxonomy singular name', 'lwn-bookcenter' ),
+		'search_items'               => __( 'Search Publishers', 'lwn-bookcenter' ),
+		'popular_items'              => __( 'Popular Publishers', 'lwn-bookcenter' ),
+		'all_items'                  => __( 'All Publishers', 'lwn-bookcenter' ),
+		'parent_item'                => null,
+		'parent_item_colon'          => null,
+		'edit_item'                  => __( 'Edit Publisher', 'lwn-bookcenter' ),
+		'update_item'                => __( 'Update Publisher', 'lwn-bookcenter' ),
+		'add_new_item'               => __( 'Add New Publisher', 'lwn-bookcenter' ),
+		'new_item_name'              => __( 'New Publisher Name', 'lwn-bookcenter' ),
+		'separate_items_with_commas' => __( 'Separate Publishers with commas', 'lwn-bookcenter' ),
+		'add_or_remove_items'        => __( 'Add or remove Publishers', 'lwn-bookcenter' ),
+		'choose_from_most_used'      => __( 'Choose from the most used Publishers', 'lwn-bookcenter' ),
+		'not_found'                  => __( 'No Publishers found.', 'lwn-bookcenter' ),
+		'menu_name'                  => __( 'Publishers', 'lwn-bookcenter' ),
+	);
+
+	$publisher_args = array(
+		'hierarchical'          => false,
+		'labels'                => $publisher_labels,
+		'show_ui'               => true,
+		'show_admin_column'     => true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var'             => true,
+		'rewrite'               => array( 'slug' => 'publisher' ),
+	);
+
+	register_taxonomy( 'publisher', 'book', $publisher_args );
+}
+add_action( 'init', 'lwn_bookcenter_register_custom_taxonomies', 0 );
+
+
+
 // Register custom post types (Book)
 function lwn_bookcenter_register_book_custom_post_type() {
     $labels = array(
